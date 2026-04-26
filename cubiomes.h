842b5718310f783db097d59ea0824d91b76b3e8e
Colin <98278012+Colin-Henry@users.noreply.github.com>
2026-04-25 22:54:30 -0400

    parent directory file upload

---
+++ cubiomes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "cubiomes/noise.h"
+#include "cubiomes/layers.h"
+#include "cubiomes/biomenoise.h"
+#include "cubiomes/generator.h"
+#include "cubiomes/finders.h"
+#include "cubiomes/util.h"
+#include "cubiomes/quadbase.h"
+#include "cubiomes/terrainnoise.h"
+#include "cubiomes/xrms.h"
+#include "cubiomes/features/stronghold.h"
+#include "cubiomes/features/mineshaft.h"
+#include "cubiomes/features/piece.h"
---
+++ mainMineshafts.c
@@ -0,0 +1,56 @@
+#include "cubiomes.h"
+#include "cubiomes/loot/loot_tables.h"
+#include <stdio.h>
+
+static void print_loot(LootTableContext* ctx) {
+    for (int i = 0; i < ctx->generated_item_count; i++) {
+        ItemStack* item_stack = &(ctx->generated_items[i]);
+        printf("%s x %d\n", get_item_name(ctx, item_stack->item), item_stack->count);
+
+        for (int j = 0; j < item_stack->enchantment_count; j++) {
+            EnchantInstance* ench = &(item_stack->enchantments[j]);
+            printf("    %s %d\n", get_enchantment_name(ench->enchantment), ench->level);
+        }
+    }
+}
+
+int main()
+{
+    int mc = MC_1_16_1;
+    int64_t seed = 3055141959546LL;
+    Generator g;
+    setupGenerator(&g, mc, 0);
+    applySeed(&g, DIM_OVERWORLD, seed);
+    int max = 400;
+    Piece *pieceList = malloc(400 * sizeof(Piece));
+    StructureSaltConfig ssconf;
+
+    
+    Pos pos;
+
+
+    for (int regX = -20; regX < 20; regX++) {
+        for (int regZ = -20; regZ < 20; regZ++) {
+            if (!getStructurePos(Mineshaft, mc, seed, regX, regZ, &pos)) // reg size for mineshafts is 1 chunk
+                continue;
+            if (isViableStructurePos(Mineshaft, &g, pos.x, pos.z, 0)) {
+                printf("Structure pos: %d %d\n", pos.x, pos.z);
+                getStructureSaltConfig(Mineshaft, mc, getBiomeAt(&g, 4, pos.x, 64, pos.z), &ssconf);
+                int mineshaftPieceCount = getMineshaftLoot(pieceList, max, ssconf, mc, seed, regX, regZ);
+                //printf("Mineshaft at %d ~ %d\n", pos.x, pos.z);
+                //printf("%d\n", mineshaftPieceCount);
+
+                for (int i = 0; i < mineshaftPieceCount; ++i) {
+                    
+                    Piece *piece = &pieceList[i];
+                    for (int j = 0; j < piece->chestCount; ++j) {
+                        Pos chestPos = piece->chestPoses[j];
+                        //printf("finalized chestpos %d %d\n", chestPos.x, chestPos.z);
+                        //printf("%lu\n\n", piece->lootSeeds);
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
