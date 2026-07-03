d5ee3c157a05970cc74fcfcb3110db97c2b9622b
Colin <98278012+Colin-Henry@users.noreply.github.com>
2026-07-03 13:47:26 -0400

    Fixed neighbor bug in water poses

--- cubiomes/finders.c
+++ cubiomes/finders.c
@@ -22,25 +22,6 @@
 
 #define PI 3.14159265358979323846
 
-// Java's Mth.sin/cos use a 65536-entry float lookup table, not the math library.
-// Canyon carver path computation uses these, so we must match them exactly.
-static float MC_SIN_TABLE[65536];
-static int mc_sin_table_init = 0;
-
-static void mc_sin_init(void) {
-    for (int i = 0; i < 65536; i++)
-        MC_SIN_TABLE[i] = (float)sin(i * M_PI * 2.0 / 65536.0);
-    mc_sin_table_init = 1;
-}
-
-static inline float mc_sin(float angle) {
-    return MC_SIN_TABLE[(int)(angle * 10430.378f) & 65535];
-}
-
-static inline float mc_cos(float angle) {
-    return MC_SIN_TABLE[((int)(angle * 10430.378f) + 16384) & 65535];
-}
-
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
@@ -1440,13 +1421,13 @@ Pos getSpawn(const Generator *g)
 // Simulating ore generation
 //==============================================================================
 
-static const uint32_t BASE_STONE_OVERWORLD_REPLACEABLES[] = {STONE, GRANITE, DIORITE, ANDESITE, DEEPSLATE, TUFF};
-static const uint32_t BASE_STONE_NETHER_REPLACEABLES[] = {NETHERRACK, BASALT, BLACKSTONE};
-static const uint32_t STONE_REPLACEABLES[] = {STONE};
-static const uint32_t NETHERRACK_REPLACEABLES[] = {NETHERRACK};
-
 int getOreConfig(int oreType, int mc, int biomeID, OreConfig *oconf)
 {
+    static const uint32_t BASE_STONE_OVERWORLD_REPLACEABLES[] = {STONE, GRANITE, DIORITE, ANDESITE, DEEPSLATE, TUFF};
+    static const uint32_t BASE_STONE_NETHER_REPLACEABLES[] = {NETHERRACK, BASALT, BLACKSTONE};
+    static const uint32_t STONE_REPLACEABLES[] = {STONE};
+    static const uint32_t NETHERRACK_REPLACEABLES[] = {NETHERRACK};
+
     // check the step in BiomeDefaultFeatures.java
     // check the size and discardChanceOnAirExposure in OreFeatures.java
     // check the repeatCount in OrePlacements.java
@@ -2377,7 +2358,7 @@ int isViableCaveBiome(int caveCarverType, int biome) {
     case OCEAN_CAVE_CARVER:
         return isOceanic(biome);
     case UNDERWATER_CAVE_CARVER:
-        return isDeepOcean(biome) || biome == frozen_ocean;
+        return isOceanic(biome);
     default:
         fprintf(stderr, "ERR isViableCaveBiome: unsupported cave carver type %d\n", caveCarverType);
         return 0;
@@ -2517,15 +2498,15 @@ static void carveCanyonInner(CanyonCarverConfig ccc, int mc, uint64_t *rnd, int 
     float g = 0.0F;
 
     for (int branchIndex = 0; branchIndex < branchCount; branchIndex++) {
-        double horizontalRadius = 1.5 + mc_sin(branchIndex * (float) PI / branchCount) * thickness;
+        double horizontalRadius = 1.5 + sin(branchIndex * (float) PI / branchCount) * thickness;
         double verticalRadius = horizontalRadius * horizontalVerticalRatio;
         horizontalRadius *= ccc.horizontalRadiusFactor(rnd, ccc.minHorRadius, ccc.maxHorRadius);
         verticalRadius = updateVerticalRadius(ccc, rnd, verticalRadius, branchCount, branchIndex);
-        float h = mc_cos(pitch);
-        float j = mc_sin(pitch);
-        x += mc_cos(yaw) * h;
+        float h = cos(pitch);
+        float j = sin(pitch);
+        x += cos(yaw) * h;
         y += j;
-        z += mc_sin(yaw) * h;
+        z += sin(yaw) * h;
         pitch *= 0.7F;
         pitch += g * 0.05F;
         yaw += f * 0.05F;
@@ -2684,7 +2665,7 @@ static void carveEllipsoid(int chunkX, int chunkZ, double x, double y, double z,
     }
     const double floorMinX = floor(x - horizontalRadius) - startChunkX - 1;
     const int minX = MAX(floorMinX, 0);
-    const double floorMaxX = floor(x + horizontalRadius) - startChunkX;
+    const double floorMaxX = floor(x + horizontalRadius) - startChunkX + 1;
     const int maxX = MIN(floorMaxX, 15);
     const double floorMinY = floor(y - verticalRadius) - 1;
     const int minY = MAX(floorMinY, worldMinY + 1);
@@ -2693,7 +2674,7 @@ static void carveEllipsoid(int chunkX, int chunkZ, double x, double y, double z,
     const int maxY = MIN(floorMaxY, worldMinY + worldHeight - 1 - n);
     const double floorMinZ = floor(z - horizontalRadius) - startChunkZ - 1;
     const int minZ = MAX(floorMinZ, 0);
-    const double floorMaxZ = floor(z + horizontalRadius) - startChunkZ;
+    const double floorMaxZ = floor(z + horizontalRadius) - startChunkZ + 1;
     const int maxZ = MIN(floorMaxZ, 15);
 
     for (int relX = minX; relX <= maxX; relX++) {
@@ -2716,7 +2697,6 @@ static void carveEllipsoid(int chunkX, int chunkZ, double x, double y, double z,
 }
 
 void applyAllCarvers(Generator *g, int chunkX, int chunkZ, Pos3List* poses, Pos3List* waterPoses) {
-    if (!mc_sin_table_init) mc_sin_init();
     int worldHeight;
     if (g->mc > MC_1_17_1) {
         worldHeight = g->dim == DIM_OVERWORLD ? 384 : 128;
@@ -2724,8 +2704,10 @@ void applyAllCarvers(Generator *g, int chunkX, int chunkZ, Pos3List* poses, Pos3
         worldHeight = g->dim == DIM_OVERWORLD ? 256 : 128;
     }
     int slots = BITNSLOTS(256 * worldHeight);
-    char carvingMask[slots];
-    memset(carvingMask, 0, slots);
+    char airCarvingMask[slots];
+    char waterCarvingMask[slots];
+    memset(airCarvingMask, 0, slots);
+    memset(waterCarvingMask, 0, slots);
 
     // TODO: replace with genBiomes?
     int biomes[17][17];
@@ -2735,14 +2717,12 @@ void applyAllCarvers(Generator *g, int chunkX, int chunkZ, Pos3List* poses, Pos3
         }
     }
 
-    int targetBiome = biomes[8][8];
-
     for (int relChunkX = -8; relChunkX <= 8; ++relChunkX) {
         for (int relChunkZ = -8; relChunkZ <= 8; ++relChunkZ) {
             int offsetChunkX = chunkX + relChunkX;
             int offsetChunkZ = chunkZ + relChunkZ;
+            int biome = biomes[relChunkZ + 8][relChunkX + 8];
 
-            int sourceBiome = biomes[relChunkZ + 8][relChunkX + 8];
             for (int canyonCarverType = 0; canyonCarverType < CANYON_CARVER_NUM; ++canyonCarverType) {
                 CanyonCarverConfig ccc;
                 if (!getCanyonCarverConfig(canyonCarverType, g->mc, &ccc)) {
@@ -2751,37 +2731,76 @@ void applyAllCarvers(Generator *g, int chunkX, int chunkZ, Pos3List* poses, Pos3
                 if (ccc.dim != g->dim) {
                     continue;
                 }
-                if (!isViableCanyonBiome(canyonCarverType, sourceBiome)) {
+                if (!isViableCanyonBiome(canyonCarverType, biome)) {
                     continue;
                 }
                 uint64_t rnd;
                 if (!checkCanyonStart(g->seed, offsetChunkX, offsetChunkZ, ccc, &rnd)) {
                     continue;
                 }
-                int prevWaterSize = waterPoses->size;
-                if (canyonCarverType == UNDERWATER_CANYON_CARVER) carveCanyonInner(ccc, g->mc, &rnd, chunkX, chunkZ, offsetChunkX, offsetChunkZ, carvingMask, waterPoses);
-                else carveCanyonInner(ccc, g->mc, &rnd, chunkX, chunkZ, offsetChunkX, offsetChunkZ, carvingMask, poses);
+                if (canyonCarverType == UNDERWATER_CANYON_CARVER) carveCanyonInner(ccc, g->mc, &rnd, chunkX, chunkZ, offsetChunkX, offsetChunkZ, waterCarvingMask, waterPoses);
+                else carveCanyonInner(ccc, g->mc, &rnd, chunkX, chunkZ, offsetChunkX, offsetChunkZ, airCarvingMask, poses);
             }
 
             for (int caveCarverType = 0; caveCarverType < CAVE_CARVER_NUM; ++caveCarverType) {
                 CaveCarverConfig ccc;
-                if (!getCaveCarverConfig(caveCarverType, g->mc, targetBiome, &ccc)) {
+                if (!getCaveCarverConfig(caveCarverType, g->mc, biome, &ccc)) {
                     continue;
                 }
                 if (ccc.dim != g->dim) {
                     continue;
                 }
-                if (!isViableCaveBiome(caveCarverType, targetBiome)) {
+                if (!isViableCaveBiome(caveCarverType, biome)) {
                     continue;
                 }
                 uint64_t rnd;
                 if (!checkCaveStart(g->seed, offsetChunkX, offsetChunkZ, ccc, &rnd)) {
                     continue;
                 }
-                int prevWaterSize2 = waterPoses->size;
-                if (caveCarverType == OCEAN_CAVE_CARVER || caveCarverType == UNDERWATER_CAVE_CARVER) carveCaveInner(ccc, &rnd, chunkX, chunkZ, offsetChunkX, offsetChunkZ, g->mc, carvingMask, waterPoses);
-                else carveCaveInner(ccc, &rnd, chunkX, chunkZ, offsetChunkX, offsetChunkZ, g->mc, carvingMask, poses);
+                if (caveCarverType == OCEAN_CAVE_CARVER || caveCarverType == UNDERWATER_CAVE_CARVER) carveCaveInner(ccc, &rnd, chunkX, chunkZ, offsetChunkX, offsetChunkZ, g->mc, waterCarvingMask, waterPoses);
+                else carveCaveInner(ccc, &rnd, chunkX, chunkZ, offsetChunkX, offsetChunkZ, g->mc, airCarvingMask, poses);
             }
+        }
+    }
+
+
+    int xDirection[4] = {0, 1, 0, -1}; // N E S W
+    int zDirection[4] = {-1, 0, 1, 0};
+    int chunkStartX = chunkX << 4;
+    int chunkStartZ = chunkZ << 4;
+
+    for (int i = 0; i < waterPoses->size; i++) {
+        int waterX = waterPoses->pos3s[i].x;
+        int waterY = waterPoses->pos3s[i].y;
+        int waterZ = waterPoses->pos3s[i].z;
+
+        if (waterY <= 10)
+            continue;
+
+        int lastX = waterX;
+        int lastZ = waterZ;
+        int placed = 0;
+        for (int direction = 0; direction < 4; direction++) {
+            int nx = waterX + xDirection[direction];
+            int nz = waterZ + zDirection[direction];
+            int nRelX = nx - chunkStartX;
+            int nRelZ = nz - chunkStartZ;
+
+            if (nRelX < 0 || nRelX > 15 || nRelZ < 0 || nRelZ > 15) {
+                waterPoses->pos3s[i] = (Pos3){lastX, waterY, lastZ};
+                placed = 1;
+                break;
+            }
+            lastX = nx;
+            lastZ = nz;
+            if (getCarveMask(airCarvingMask, nRelX, waterY, nRelZ, 0)) {
+                waterPoses->pos3s[i] = (Pos3){nx, waterY, nz};
+                placed = 1;
+                break;
+            }
+        }
+        if (!placed) {
+            waterPoses->pos3s[i] = (Pos3){waterX, waterY, waterZ};
         }
     }
 }
@@ -4048,6 +4067,7 @@ int getLootTableCountForStructure(int structure, int mc) {
     case End_Island: return 0;
     case Trail_Ruins: return 0;
     case Trial_Chambers: return 13;
+    case Stronghold: return 3;
     default:
         fprintf(stderr, "getLootTableCountForStructure: not implemented for structure %s.\n", struct2str(structure));
         exit(1);
@@ -7828,6 +7848,10 @@ static const int g_biome_para_range_215_diff[][13] = {
 {pale_garden             , -1500, 2000,  3000, IMAX,   300, IMAX, -7799,  500,  IMIN, IMAX,  IMIN, IMAX},
 {-1,0,0,0,0,0,0,0,0,0,0,0,0}};
 
+static const int g_biome_para_range_262_diff[][13] = {
+{sulfur_caves            ,  IMIN, IMAX,  IMIN, IMAX,  IMIN, IMAX,  IMIN, IMAX,  2000, 9000,  IMIN,-9500},
+{-1,0,0,0,0,0,0,0,0,0,0,0,0}};
+
 /**
  * Gets the min/max parameter values within which a biome change can occur.
  */
@@ -7865,6 +7889,14 @@ const int *getBiomeParaLimits(int mc, int id)
     if (mc <= MC_1_17)
         return NULL;
     int i;
+    if (mc > MC_26_1)
+    {
+        for (i = 0; g_biome_para_range_262_diff[i][0] != -1; i++)
+        {
+            if (g_biome_para_range_262_diff[i][0] == id)
+                return &g_biome_para_range_262_diff[i][1];
+        }
+    }
     if (mc > MC_1_21_4)
     {
         for (i = 0; g_biome_para_range_215_diff[i][0] != -1; i++)
